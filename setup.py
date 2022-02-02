86270e44880885f608645958132664f4c4be5799
Martin Thoma <info@martin-thoma.de>
2022-02-02 21:12:49 +0100

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -5,20 +5,4 @@
 # Third party modules
 from setuptools import setup
 
-setup(
-    package_data={"shell_history_analysis": ["grouping.yaml"]},
-    install_requires=[
-        "click",
-        "python-dateutil",
-        "matplotlib",
-        "pandas",
-        "pyaml",
-    ],
-    tests_require=[
-        "pytest",
-        "pytest-cov",
-        "pytest-mccabe",
-        "pytest-flake8",
-        "simplejson",
-    ],
-)
+setup()
